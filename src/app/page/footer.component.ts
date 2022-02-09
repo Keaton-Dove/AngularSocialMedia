25831fe21b55b877aba121d44df9df98b044cabe
Keaton <keatondove10@gmail.com>
2022-02-08 22:16:51 -0500

    Init header and footer files

---
+++ src/app/page/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-footer',
+    templateUrl: './footer.component.html'
+})
+
+export class FooterComponent {
+    x: number = 1;
+}
---
+++ src/app/page/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'page-header',
+    templateUrl: './header.component.html'
+})
+
+export class HeaderComponent {
+    x: number = 1;
+}
