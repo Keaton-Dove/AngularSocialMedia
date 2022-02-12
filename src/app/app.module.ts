85f239a8ed246cfa01139fc7e8410cffdbc8bcdc
Keaton <keatondove10@gmail.com>
2022-02-12 13:02:57 -0500

    Creating toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent, FooterComponent } from './page';
+import { AppRoutingModule } from './app-routing.module';
+import { NavbarComponent } from './page/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/page/index.ts
@@ -0,0 +1,3 @@
+// Index file for exporting header and footer files from page folder
+export * from './footer.component';
+export * from './header.component';
